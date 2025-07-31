e90162eb879c9c411b43753225009c2576024a8c
Niralidave19 <niralidave2019@gmail.com>
2025-07-31 09:49:25 +0530

    Added flashing code

--- app/source/app_uart_flash.c
+++ app/source/app_uart_flash.c
@@ -6,8 +6,7 @@
 
 #include <stdint.h>
 #include "uart_proto.h"
-#include "app_uart_flash_proto.h"
-
+#include "app_uart_proto.h"
 /* Receive request to flash data from client
  * Send ACK to client
  * Client sends no. of bytes of data
@@ -17,3 +16,21 @@
  * Server sends ACK
  * Clients sends data ... process continues till no. of bytes reached
  */
+
+void app_uart_flash_update(){
+	/* receive the request frame ID */
+	uint8_t rx_buff = app_req_frame();
+	uint32_t size;
+	/* if request frame ID == 0x3
+	 * Flash software */
+	if(rx_buff == 0x3){
+		/*send an ACK*/
+		app_send_ACK();
+	}
+	/* Client sends total size of flash file */
+
+	/*sector erase*/
+
+	/* Server sends ACK indicating flash sector is erased - send data */
+	app_send_ACK();
+}
